70f0a3ddc9fb16000240ceb8bd092c984c4c01ae
Nace Zupančič <zupancicnace97@gmail.com>
2024-10-16 21:13:48 +0200

    implement MVVM pattern; add viewModel, RelayCommand and some basic bindings

---
+++ MESI-APP/ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using MESI_APP.Commands;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Input;
+
+namespace MESI_APP.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {        
+        public ICommand SaveSettingsCommand{ get; set; }
+        private string _serverOutboundUrl;
+        public string ServerOutboundUrl
+        {
+            get => _serverOutboundUrl;
+            set
+            {
+                _serverOutboundUrl = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+            ServerOutboundUrl = "Binding works";
+            SaveSettingsCommand = new RelayCommand(execute => SaveSettings());
+        }
+
+        private void SaveSettings() {
+            ServerOutboundUrl += "Yay";
+        }
+    }
+}
---
+++ MESI-APP/Views/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using MESI_APP.ViewModels;
+using System.Windows;
+
+namespace MESI_APP.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainViewModel _mainViewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _mainViewModel = new MainViewModel();
+            this.DataContext = _mainViewModel;
+        }
+    }
+}
