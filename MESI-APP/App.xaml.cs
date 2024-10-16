fee6333f4c82d7efb2c54d53350e5102bf29ce03
Nace Zupančič <zupancicnace97@gmail.com>
2024-10-16 14:50:22 +0200

    Add fresh WPF project and gitignore file

---
+++ MESI-APP/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MESI_APP
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
