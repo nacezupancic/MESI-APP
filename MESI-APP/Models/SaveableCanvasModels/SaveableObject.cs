f3bd13bc95606ed879cac578b1936154c00c8f49
Nace Zupančič <zupancicnace97@gmail.com>
2024-10-24 00:05:29 +0200

    revert button styling

--- MESI-APP/Models/SaveableCanvasModels/SaveableObject.cs
+++ MESI-APP/Models/SaveableCanvasModels/SaveableObject.cs
@@ -1,14 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MESI_APP.Models.SaveableCanvasModels
+﻿namespace MESI_APP.Models.SaveableCanvasModels
 {
-    public class SaveableObject    {
+    public class SaveableObject {
         
         public string BindableName { get; set; }
     }
